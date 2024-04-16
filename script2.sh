07570eb1a42e16e985f4c3baf0c7d1a946768759
vasim <aneeshaanu4578@gmail.com>
2024-04-16 10:42:09 +0530

    first message

---
+++ script2.sh
@@ -0,0 +1,7 @@
+
+#by default if we dont define the shebang it will while executing it will take the shell that we execute.
+
+#  today we are going to learn variables
+name=vasim
+
+echo "hi my name is $name"
