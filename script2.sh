ba8d5ae92fafa78e96c7c68de4b011b9c3ffe3d7
vasim <aneeshaanu4578@gmail.com>
2024-04-16 17:53:53 +0530

    first message

--- script2.sh
+++ script2.sh
@@ -2,30 +2,12 @@
 #by default if we dont define the shebang it will while executing it will take the shell that we execute.
 
 #  today we are going to learn variables
-a=10
-b=xyz
-c=true
-d=abc123
 
-aa=1
-bb=2
+#before execution
 
-name=(vasim mutahar samtosh)
+read  -p "enter your name :" name
 
-DATE=$(date +%F)
-
-echo "hi my name is $name"
-
-echo "my name is $DATE"
-echo "my name is $b"
-echo "my name is $c"
-echo "my name is $d"
-
-echo "addition = $((aa+bb))"
-
-echo "my name is ${name[1]}"
-echo "my name is ${name[2]}"
-echo "my name is ${name[n]}"
+echo "my name is $name"
 
 
 
