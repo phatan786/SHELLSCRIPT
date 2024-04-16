c5edd24adb355ef9988d476862395bc914dc722b
vasim <aneeshaanu4578@gmail.com>
2024-04-16 10:35:30 +0530

    first message

--- script1.sh
+++ script1.sh
@@ -3,5 +3,5 @@
 # This is used to comment the lines inside the shell script
 echo -e "hello\nworld"
 echo -e "hello\tworld"
-echo -e "\e[1;34mhelloworld"
+echo -e "\e[1;34mhelloworld\e[0m"
 echo "hello world"
